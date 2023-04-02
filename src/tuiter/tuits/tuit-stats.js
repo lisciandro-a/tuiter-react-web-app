7cedb89929c45465e778c670f1aadd0bdd1ce360
Allison Lisciandro <80727184+lisciandro-a@users.noreply.github.com>
2023-04-02 04:49:25 -0400

    a8 REST fa22 - add dislike

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -4,9 +4,11 @@ import {
   faRetweet,
   faShareNodes,
   faHeart as faHeartLiked,
+  faThumbsDown as faThumbsDownUnliked,
 } from "@fortawesome/free-solid-svg-icons";
 import {
   faHeart as faHeartUnliked,
+  faThumbsDown as faThumbsDownUnDisliked,
   faComment,
 } from "@fortawesome/free-regular-svg-icons";
 import { useDispatch } from "react-redux";
@@ -21,23 +23,30 @@ const TuitStats = ({ tuit }) => {
       liked: !tuit.liked
     }));
   };
+  const toggleDislikeTuitHandler = (tuit) => {
+    dispatch(updateTuitThunk({
+      ...tuit,
+      dislikes: !tuit.disliked ? tuit.dislikes + 1 : tuit.dislikes - 1,
+      disliked: !tuit.disliked
+    }));
+  };
   return (
     <div className="row mt-2">
-      <div className="col-3">
+      <div className="col">
         <button onClick={() => {}} className="btn">
           <FontAwesomeIcon icon={faComment} />
           <span className="ms-2">{tuit.replies}</span>
         </button>
       </div>
 
-      <div className="col-3">
+      <div className="col">
         <button onClick={() => {}} className="btn">
           <FontAwesomeIcon icon={faRetweet} />
           <span className="ms-2">{tuit.retuits}</span>
         </button>
       </div>
 
-      <div className="col-3">
+      <div className="col">
         <button onClick={() => toggleLikeTuitHandler(tuit)} className="btn">
           <FontAwesomeIcon
             icon={tuit.liked ? faHeartLiked : faHeartUnliked}
@@ -47,7 +56,17 @@ const TuitStats = ({ tuit }) => {
         </button>
       </div>
 
-      <div className="col-3">
+      <div className="col">
+        <button onClick={() => toggleDislikeTuitHandler(tuit)} className="btn">
+          <FontAwesomeIcon
+            icon={tuit.disliked ? faThumbsDownUnliked : faThumbsDownUnDisliked}
+            className={tuit.disliked ? "text-primary" : "text-dark"}
+          />
+          <span className="ms-2">{tuit.dislikes}</span>
+        </button>
+      </div>
+
+      <div className="col-2">
         <button onClick={() => {}} className="btn">
           <FontAwesomeIcon icon={faShareNodes} />
         </button>
